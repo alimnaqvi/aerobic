fb3524e73d155a926190dee3117be15c266ce8e3
Ali Mohsin Naqvi <alimohsinn@hotmail.com>
2026-01-01 19:07:40 +0100

    remove elevation of the zone filter container to improve aesthetic

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -388,7 +388,7 @@ const styles = StyleSheet.create({
     shadowOffset: { width: 0, height: 2 },
     shadowOpacity: 0.1,
     shadowRadius: 8,
-    elevation: 5,
+    // elevation: 5,
     borderWidth: 1,
     borderColor: 'transparent',
     gap: 10,
